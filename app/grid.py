60f06d75a45923126c175941a30f8a5bc215a51b
willcard <william.cardoso.pro@gmail.com>
2024-10-15 21:56:53 +0200

    init grid with pretty_print method

--- app/grid.py
+++ app/grid.py
@@ -1,5 +1,24 @@
 class Grid:
-    pass
+    def __init__(self, width:int =10, height:int =10) -> None:
+        self.width = width
+        self.height = height
+        self.grid_values = [[0 for w in range(width)] for h in range(height)]
+
+    def pretty_print(self,) -> None:
+        """
+            Tool to return a str view of the grid.
+        """
+        _sep = '+' + '---+'*self.width + '\n'
+        pretty = _sep
+
+        for row in self.grid_values:
+            _row = '|'
+            for cell in row:
+                _row += f' {cell} |'
+            pretty += _row + '\n' + _sep
+
+        return pretty
+
 
 
 class Apple:
---
+++ app/main.py
@@ -0,0 +1,8 @@
+from grid import Grid
+
+def main():
+    grid = Grid(5,5)
+    print(grid.pretty_print())
+
+if __name__ == "__main__":
+    main()
