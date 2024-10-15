48a2a48a9114ed7a3baa094ea755a05f5c566346
willcard <william.cardoso.pro@gmail.com>
2024-10-16 00:20:18 +0200

    init apple and snake on the grid

--- app/grid.py
+++ app/grid.py
@@ -1,11 +1,19 @@
+import random
+
 class Grid:
     def __init__(self, width:int =10, height:int =10) -> None:
         self.width = width
         self.height = height
         self.grid_values = [[' ' for w in range(width)] for h in range(height)]
 
-        self.apple = Apple()
         self.snake = Snake()
+        self.update_snake(first=True)
+
+        self.apple = Apple()
+        self.apple.new_position(limits=(self.width-1,self.height-1),
+                                        exclude= [self.snake.get_head_position()])
+        self.update_apple(first=True)
+
 
     def pretty_print(self,) -> str:
         """
@@ -22,18 +30,52 @@ class Grid:
 
         return pretty
     
-    def update_apple(self, width, height) -> None:
-        pass
+    def update_apple(self, first=False) -> None:
+        if first:
+            _w,_h = self.apple.get_position()
+            self.grid_values[_w][_h] = 'O'
+        else:
+            #TODO
+            pass
+
+    def update_snake(self, first=False) -> None:
+        if first:
+            _w,_h = self.snake.get_head_position()
+            self.grid_values[_w][_h] = self.snake.MARKER
+        else:
+            #TODO
+            pass
 
 
 class Snake:
-    DIRECTIONS = ('UP','DOWN','LEFT','RIGHT')
+    ALLOWED_DIRECTIONS = ('UP','DOWN','LEFT','RIGHT')
+    MARKER = '#'
     def __init__(self) -> None:
         self.length = 1
-        self.position = (0,0)
-        self.direction = 'UP'
+        self.head_position = (0,0)
+        self.tail_position = self.head_position
+        self.direction = 'DOWN'
 
+    def grow_up(self) -> None:
+        pass
+
+    def get_head_position(self) -> tuple[int,int]:
+        return self.head_position
+    
 
 class Apple:
+    MARKER = 'O'
     def __init__(self) -> None:
-        self.position = (2,2)
+        self.position = (1,1)
+
+    def new_position(self, limits:tuple[int,int], exclude=list[tuple[int,int]]) -> None:
+        isok = False
+        while not isok:
+            _w = random.randint(0,limits[0])
+            _h = random.randint(0,limits[1])
+            if not (_w,_h) in exclude:
+                isok = True
+        self.position = (_w,_h)
+
+    def get_position(self) -> tuple[int,int]:
+        return self.position
