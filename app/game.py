beb15b633371fd16ac4bf69087644206019f129c
willcard <william.cardoso.pro@gmail.com>
2024-10-16 20:00:16 +0200

    create conf and init game class

--- app/game.py
+++ app/game.py
@@ -1,2 +1,11 @@
+import conf
+from grid import Grid
+
 class Game:
-    pass
+    def __init__(self) -> None:
+        self.grid = Grid(conf.GRID_SHAPE)
+
+    def run(self) -> None:
+        print(self.grid.pretty_print())
+        self.grid.snake.move('UP')
+        print(self.grid.snake_is_out())
--- app/main.py
+++ app/main.py
@@ -1,12 +1,8 @@
-from grid import Grid
+from game import Game
 
 def main():
-    grid = Grid(5,5)
-    print(grid.pretty_print())
-
-    grid.snake.move('UP')
-    print(grid.snake_is_out())
-
+    game = Game()
+    game.run()
 
 if __name__ == "__main__":
     main()
