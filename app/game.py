24b9dabf9bfe438df1865ed50c9f1e645ed1d06f
willcard <william.cardoso.pro@gmail.com>
2024-10-20 00:13:10 +0200

    add pygame keyboard controls

--- app/game.py
+++ app/game.py
@@ -1,24 +1,27 @@
 import conf
 from grid import Grid
+import pygame
 import time
 
 class Game:
     def __init__(self) -> None:
+        self.screen = pygame.display.set_mode((400, 300))
+        pygame.init()
+
         self.grid = Grid(*conf.GRID_SHAPE)
         print(self.grid.pretty_print())
 
-        self.TEST_MOVES = ['DOWN','RIGHT','DOWN','RIGHT','DOWN','RIGHT','DOWN','RIGHT',
-                           'UP','LEFT','UP','LEFT','UP','LEFT','UP','LEFT',
-                           'DOWN','RIGHT','DOWN','RIGHT','DOWN','RIGHT','DOWN','RIGHT',
-                           'UP','LEFT','UP','LEFT','UP','LEFT','UP','LEFT']
-
-
     def start(self) -> None:
-        while self.TEST_MOVES:
-            move = self.get_user_move()
-            self.grid.snake_move(move)
-            print(self.grid.pretty_print())
-            time.sleep(0.5)
-
-    def get_user_move(self) -> str:
-        return self.TEST_MOVES.pop(0)
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        self.grid.snake_move('UP')
+                    elif event.key == pygame.K_DOWN:
+                        self.grid.snake_move('DOWN')
+                    elif event.key == pygame.K_LEFT:
+                        self.grid.snake_move('LEFT')
+                    elif event.key == pygame.K_RIGHT:
+                        self.grid.snake_move('RIGHT')
+                    print(self.grid.pretty_print())
+                    #time.sleep(0.5)
