30da31bc4b7e36810be45058d14c2d38ad186b1d
willcard <william.cardoso.pro@gmail.com>
2024-10-19 22:34:52 +0200

    init fixed test run

--- app/game.py
+++ app/game.py
@@ -4,14 +4,9 @@ from grid import Grid
 class Game:
     def __init__(self) -> None:
         self.grid = Grid(*conf.GRID_SHAPE)
-
-    def run(self) -> None:
         print(self.grid.pretty_print())
 
-        self.grid.snake.move('DOWN')
-        self.grid.update()
-        print(self.grid.pretty_print())
-
-        self.grid.snake.move('DOWN')
+    def run(self, move) -> None:
+        self.grid.snake.move(move)
         self.grid.update()
         print(self.grid.pretty_print())
--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 from game import Game
 
+TEST_MOVES = ('DOWN','DOWN','RIGHT','DOWN','RIGHT')
+
 def main():
     game = Game()
-    game.run()
+
+    for move in TEST_MOVES:
+        game.run(move)
 
 if __name__ == "__main__":
     main()
